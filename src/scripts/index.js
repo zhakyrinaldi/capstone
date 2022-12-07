a268a418cf9a7335a331ed8d49f551707f21d27c
arielcatur <arielcatur1305@gmail.com>
2022-12-07 20:10:28 +0800

    update by ariel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import 'regenerator-runtime';
 import './component/nav-bar.js';
 import './component/about-us.js';
+import '../styles/style.css';
 document.addEventListener('DOMContentLoaded', main);
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,20 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.config');
+const common = require('./webpack.common');
  
 module.exports = merge(common, {
-  mode: 'development'
-})
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 9000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    compress: true,
+  },
+});
